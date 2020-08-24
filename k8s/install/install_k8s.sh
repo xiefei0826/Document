5458a89fd229ba84bfb85b9c9e6df6ed6c72f9dd
xiefei0826 <46474161+xiefei0826@users.noreply.github.com>
2020-08-24 08:05:58 +0800

    Update install_k8s.sh

--- k8s/install/install_k8s.sh
+++ k8s/install/install_k8s.sh
@@ -6,6 +6,7 @@ EOF
 # 安装k8s
 sudo apt-get update && sudo apt-get install -y  kubeadm 
 #sudo kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr=10.244.0.0/16 --image-repository=registry.cn-hangzhou.aliyuncs.com/officialproject
+#registry.cn-hangzhou.aliyuncs.com/google_containers
 #sudo kubectl apply -f kube-flannel.yml
 #启用主控机同时作为工作节点
 #sudo kubectl taint nodes --all node-role.kubernetes.io/master-
