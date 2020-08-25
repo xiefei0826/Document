6e6d98f24f9e292394fe243b46945692c1ee1041
xf <280755491@qq.com>
2020-08-25 21:03:59 +0800

    k8s

--- k8s/install/install_k8s.sh
+++ k8s/install/install_k8s.sh
@@ -4,16 +4,15 @@ EOF
 # 安装k8s
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y  kubeadm 
+sudo apt-get update && sudo apt-get install -y  kubeadm
 
-sudo kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr=10.244.0.0/16 --image-repository=registry.cn-hangzhou.aliyuncs.com/officialproject
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
-sudo kubectl apply -f kube-flannel.yml
+#sudo kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr=10.244.0.0/16 --image-repository=registry.cn-hangzhou.aliyuncs.com/officialproject
+#registry.cn-hangzhou.aliyuncs.com/google_containers
+#mkdir -p $HOME/.kube
+#sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+#
+#sudo kubectl apply -f kube-flannel.yml
 
 #启用主控机同时作为工作节点
 # kubectl taint nodes --all node-role.kubernetes.io/master-
