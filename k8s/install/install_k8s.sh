3d0af7934e727dd6eab8361f38b8e2569cad52a7
xf <280755491@qq.com>
2020-09-08 14:35:58 +0800

    k8s 网络设置

--- k8s/install/install_k8s.sh
+++ k8s/install/install_k8s.sh
@@ -6,6 +6,11 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y  kubeadm
 
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
 #sudo kubeadm init --apiserver-advertise-address=192.168.1.131 --pod-network-cidr=10.244.0.0/16 --image-repository=registry.cn-hangzhou.aliyuncs.com/officialproject
 #registry.cn-hangzhou.aliyuncs.com/google_containers
 #mkdir -p $HOME/.kube
